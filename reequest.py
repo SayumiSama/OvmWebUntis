06765f196880242a4d5a3b8a1ce33f90abdeac86
Louis Genzel <l.genzel@micromata.de>
2022-09-21 09:39:26 +0200

    Added Timetable request and Room Names

--- reequest.py
+++ reequest.py
@@ -1,4 +1,10 @@
 import webuntis
+import datetime
+
+
+today = datetime.date.today()
+monday = today - datetime.timedelta(days=today.weekday())
+friday = monday + datetime.timedelta(days=4)
 
 file = open("/Users/louisgenzel/Documents/Coding/secrets/webuntissecret", "r")
 secret = file.read()
@@ -10,7 +16,8 @@ s = webuntis.Session(
     username='GENZEL_20030211',
     password= apiToken,
     school='oskar-von-miller-kassel',
-    useragent='WebUntis'
+    useragent='lgenzel',
+    use_cache= True    
 )
 
 s.login()
@@ -18,10 +25,21 @@ s.login()
 for klasse in s.klassen():
     print(klasse.name)
 
-
 for teachers in s.teachers():
     print(teachers.full_name)
 
-
 for students in s.students():
-    print(students.full_name)    
+    print(students.full_name)   
+
+for years in s.schoolyears():
+    print(years.name)
+
+for Rooms in s.rooms():
+    print(Rooms.name)
+
+klasse = s.klassen().filter(name={'11FId'})
+
+for tt in klasse:
+    print(tt.long_name)
+
+s.logout()
